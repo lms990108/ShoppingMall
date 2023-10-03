ad69382cb19b13497eda67f3e6bdb76e23b8b162
HyoIn Kim <jeongh.y2306@gmail.com>
2023-10-03 21:27:02 +0900

    [test] 프로젝트 초기 세팅 (1)

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/health", (req, res) => {
+  res.end("Server is on");
+});
+
+const app = express();
+
+app.use(express.static("views"));
+
+app.use("/apis", router);
+
+app.use((req, res) => {
+  res.end("Not Found");
+});
+
+app.use((err, req, res, next) => {
+  res.json({ code: 0, message: err.message, response: {} });
+});
+
+app.listen(3000, () => {
+  console.log("서버 시작: http://localhost:3000");
+});
+
+/**
+ * API 명세 팁
+ *
+ * 요청 주소
+ * 메소드 GET, POST, PUT, PATCH, DELETE
+ * 요약 API의 역할
+ * [req.params] params /users/{id} => http://localhost:3000/apis/users/asndklsabj21321sdsa
+ * [req.query] querystring ?name=elice&age=8
+ * [req.body] body (application/json) => "{ "name": "string" }"
+ * require
+ *
+ * response
+ * 200 : 성공 => { code: 200, message: "success", reponse: {} }
+ * 400 : 파라미터 누락 => { code: 400, message: "Parameter Missing", reponse: {} }
+ * 403 : 인증 오류 => { code: 403, message: "User Id Missing", reponse: {} }
+ */
