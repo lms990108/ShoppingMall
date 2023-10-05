d895f1b0fd40ab4b95d764cd36c0166f19ccc7b1
최정윤 <ej921004@naver.com>
2023-10-05 13:31:07 +0900

    Feat : bulma script 추가

--- views/common/common.js
+++ views/common/common.js
@@ -1,5 +1,14 @@
-// 모든 페이지에서 공통으로 사용되는 컴포넌트를 정의하기 위한 파일입니다.
-// header와 footer가 모든 페이지에서 공통으로 사용됩니다.
+// 모든 페이지에서 공통으로 사용되는 요소들을 정의하기 위한 파일입니다.
+
+/* head 태그 */
+/* bulma 사용 */
+document.querySelector("head").innerHTML += /*html*/ `
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+    />`;
+
+/* header(navigator) */
 
 document.getElementById("header").innerHTML = /*html*/ ` 
 <div>header</div>
@@ -10,5 +19,7 @@ document.getElementById("header").innerHTML = /*html*/ `
     <li><a href="/login">로그인</a></li>
   </ul>
 </nav>`;
+
+/* footer */
 
 document.getElementById("footer").innerHTML = /*html*/ ` <div>footer</div>`;
