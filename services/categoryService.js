d8e5ab6cb6c6dccbb06dbc99ed007ec68c0c3fa0
lee minseop <lms990108@gmail.com>
2023-10-06 13:28:23 +0900

    Comment: 각 기능에 대한 명세와 설명을 주석으로 추가

--- services/categoryService.js
+++ services/categoryService.js
@@ -5,16 +5,30 @@ class categoryService {
     this.categoryModel = categoryModel;
   }
 
+  /**
+   * 모든 카테고리를 조회합니다.
+   * @returns {Array} 모든 카테고리의 배열을 반환합니다.
+   */
   async getAllCategories() {
     return await this.categoryModel.find().exec();
   }
 
+  /**
+   * 새로운 카테고리를 생성합니다.
+   * @param {Object} data - 생성할 카테고리의 데이터입니다.
+   * @returns {Array} 모든 카테고리의 배열을 반환합니다.
+   */
   async createCategory(data) {
     const category = new this.categoryModel(data);
     await category.save();
     return this.getAllCategories();
   }
 
+  /**
+   * 카테고리 이름으로 카테고리를 조회합니다.
+   * @param {string} categoryName - 조회할 카테고리의 이름입니다.
+   * @returns {Object} 조회된 카테고리를 반환합니다.
+   */
   async getCategoryByName(categoryName) {
     const category = await this.categoryModel
       .findOne({ name: categoryName })
@@ -25,6 +39,12 @@ class categoryService {
     return category;
   }
 
+  /**
+   * 카테고리 이름을 기준으로 카테고리를 수정합니다.
+   * @param {string} categoryName - 수정할 카테고리의 이름입니다.
+   * @param {Object} data - 수정할 데이터입니다.
+   * @returns {Array} 모든 카테고리의 배열을 반환합니다.
+   */
   async updateCategory(categoryName, data) {
     const updatedCategory = await this.categoryModel
       .findOneAndUpdate({ name: categoryName }, data, { new: true })
@@ -35,6 +55,11 @@ class categoryService {
     return this.getAllCategories();
   }
 
+  /**
+   * 카테고리 이름을 기준으로 카테고리를 삭제합니다.
+   * @param {string} categoryName - 삭제할 카테고리의 이름입니다.
+   * @returns {Array} 모든 카테고리의 배열을 반환합니다.
+   */
   async deleteCategory(categoryName) {
     const deletedCategory = await this.categoryModel
       .findOneAndDelete({ name: categoryName })
