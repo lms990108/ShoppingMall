d668fe1f746dd703b5a60fb5ac653f1b30ce6ce5
muleesm <muleesm@gmail.com>
2023-10-06 17:17:44 +0900

    Chore: login로직 수정, mypage_myorder 페이지 생성

--- views/login/login.js
+++ views/login/login.js
@@ -14,3 +14,29 @@ document.getElementById('login-button').addEventListener('submit', function(even
       alert('Login failed!');
   }
 });
+
+//
+// Toggle edit mode for profile fields
+document.getElementById('editButton').addEventListener('click', function() {
+  let fields = ['emailField', 'nameField', 'addressField', 'phoneField'];
+  let isEditable = fields.some(id => !document.getElementById(id).hasAttribute('readonly'));
+
+  fields.forEach(id => {
+      let field = document.getElementById(id);
+      if (isEditable) {
+          field.setAttribute('readonly', true);
+          field.classList.add('is-static');
+      } else {
+          field.removeAttribute('readonly');
+          field.classList.remove('is-static');
+      }
+  });
+
+  if (isEditable) {
+      // Handle saving logic here (e.g., sending data to the backend)
+      alert('회원정보가 변경되었습니다!');
+  }
+
+  // Toggle button text between "Edit" and "Save"
+  this.textContent = isEditable ? 'Edit' : 'Save';
+});
