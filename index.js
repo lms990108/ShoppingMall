96f672c51cb2aa5bbde0fc7b447d4cdb3fda3abf
lee minseop <lms990108@gmail.com>
2023-10-08 23:08:34 +0900

    Refactor: 코드 가독성 정리 밑 indexRouter삭제

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/** 
-[김효인 라우터 설정] - 이건 나중에 삭제할 것 같아요.
-라우터 모듈로만 사용
-const express = require("express");
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.end("Server is on");
-});
-module.export = router;
-*/
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -18,11 +8,12 @@ const { viewRouter } = require("./routes/viewRouter");
 const orderRouter = require("./routes/orderRouter");
 const productRouter = require("./routes/productRouter");
 const categoryRouter = require("./routes/categoryRouter");
-//const authApiRouter = require("./routes/authApiRouter");
-//const userApiRouter = require("./routes/userApiRouter");
+const authApiRouter = require("./routes/authApiRouter");
+const userApiRouter = require("./routes/userApiRouter");
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5001;
 const mongoURI = process.env.MONGO_DB_PATH;
 // console.log(mongoURI);
 
@@ -46,41 +37,22 @@ app.use(express.static("views")); // 정적 파일 미들웨어
 app.use("/health", (req, res) => {
   res.end("Server is on");
 });
-
-app.use("/apis", router);
-const indexRouter = require("./routes/indexRouter");
-app.use("/api", indexRouter);
-
+app.use("/user", userApiRouter); // 유저 라우터
+app.use("/auth", authApiRouter); // 회원가입 라우터
 app.use("/order", orderRouter); // 주문 라우터
 app.use("/product", productRouter); // 상품 라우터
 app.use("/category", categoryRouter); // 카테고리 라우터
+
 // Error handlers
 app.use((req, res) => {
-  res.end("Not Found");
+  res.status(404).end("Not Found");
 });
 
 app.use((err, req, res, next) => {
-  res.json({ code: 0, message: err.message, response: {} });
+  console.error(err.stack); // Add this line to log the error
+  res.status(500).json({ code: 0, message: err.message, response: {} });
 });
 
-const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`서버 시작: http://localhost:${PORT}`);
 });
-
-/**
- * API 명세 팁
- *
- * 요청 주소
- * 메소드 GET, POST, PUT, PATCH, DELETE
- * 요약 API의 역할
- * [req.params] params /users/{id} => http://localhost:3000/apis/users/asndklsabj21321sdsa
- * [req.query] querystring ?name=이름&age=8
- * [req.body] body (application/json) => "{ "name": "string" }"
- * require
- *
- * response
- * 200 : 성공 => { code: 200, message: "success", response: {} }
- * 400 : 파라미터 누락 => { code: 400, message: "Parameter Missing", response: {} }
- * 403 : 인증 오류 => { code: 403, message: "User Id Missing", response: {} }
- */
