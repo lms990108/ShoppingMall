b807d4f39b5485c0d94b82ddae23d75ca0ed8ed2
최정윤 <ej921004@naver.com>
2023-10-09 03:23:11 +0900

    Feat : 관리자페이지에 카테고리 관리, 주문 관리, 회원 관리 section 추가

--- views/admin/admin.js
+++ views/admin/admin.js
@@ -1,3 +1,23 @@
+/* tab의 값에 따라 관리하고자하는 target 화면 보여주기 */
+const tabs = Array.prototype.slice.call(
+  document.querySelectorAll("#tabs li"),
+  0,
+);
+const contents = Array.prototype.slice.call(
+  document.querySelectorAll(".content-tab"),
+  0,
+);
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((t) => t.classList.remove("is-active"));
+    tab.classList.add("is-active");
+
+    contents.forEach((c) => c.classList.remove("is-active"));
+    document.getElementById(contents[index].id).classList.add("is-active");
+  });
+});
+
 /* 상품관리 테이블 불러오기 */
 
 const productsTable = document.querySelector("tbody");
