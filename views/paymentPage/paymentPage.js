93295851e9d61f6c216ebb3fd4f2cc8f5ae1037d
최정윤 <ej921004@naver.com>
2023-10-11 04:39:10 +0900

    Feat : 결제페이지 - 주문자 정보 UI

--- views/paymentPage/paymentPage.js
+++ views/paymentPage/paymentPage.js
@@ -1,3 +1,42 @@
+const orderProduct_info = document.querySelector("#orderProduct_info");
+const orderer_info = document.querySelector("#orderer_info");
+const delivery_info = document.querySelector("#delivery_info");
+const order_summary = document.querySelector("#order_summary");
+const payment_method = document.querySelector("#payment_method");
+
+const orderProductHTML = (product) => {
+  return /*html*/ `<div class="is-flex product">
+  <div class="is-flex-grow-1">  <img class="product_img" src="${
+    product.main_img_url
+  }"/></div>
+<div class="is-flex-grow-2">  <p>${product.product_name}</p>
+  <p>개수 : <input type="number" class="input_qty" value=${product.qty}>개</p>
+  <p>총 금액 : ${product.price * product.qty} 원</p></div>
+
+  </div>`;
+};
+
+const loadOrderProductInfo = (products) => {
+  products.forEach((product) => {
+    orderProduct_info.insertAdjacentHTML(
+      "beforeend",
+      orderProductHTML(product),
+    );
+  });
+};
+
+const loadOrdererInfo = (orderer) => {
+  orderer_info.insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `<div class="m-5">
+  <label class="label has-text-weight-bold">이름 : <span>${orderer.name}</span></label>
+  <label class="label has-text-weight-bold">연락처 : <span>${orderer.contact}</span> </label>
+  <label class="label has-text-weight-bold">이메일 : <span>${orderer.email}</span></label>
+  </div>
+  `,
+  );
+};
+
 /* 주문 상품 mockData */
 const order_products = [
   {
@@ -29,31 +68,22 @@ const order_products = [
   },
 ];
 
-const orderProduct_info = document.querySelector("#orderProduct_info");
-const orderer_info = document.querySelector("#orderer_info");
-const delivery_info = document.querySelector("#delivery_info");
-const order_summary = document.querySelector("#order_summary");
-const payment_method = document.querySelector("#payment_method");
-
-const orderProductHTML = (product) => {
-  return /*html*/ `<div class="is-flex product">
-  <div class="is-flex-grow-1">  <img class="product_img" src="${
-    product.main_img_url
-  }"/></div>
-<div class="is-flex-grow-2">  <p>${product.product_name}</p>
-  <p>개수 : <input type="number" class="input_qty" value=${product.qty}개></p>
-  <p>총 금액 : ${product.price * product.qty} 원</p></div>
-
-  </div>`;
+/* 주문자 mockData */
+const orderer = {
+  name: "홍길동",
+  email: "hong123@gmail.com",
+  contact: "010-1111-1111",
+  address: "서울특별시 성동구",
+  detail_address: "123-12",
 };
 
-const loadOrderProductInfo = (products) => {
-  products.forEach((product) => {
-    orderProduct_info.insertAdjacentHTML(
-      "beforeend",
-      orderProductHTML(product),
-    );
-  });
+const delivery = {
+  name: "",
+  email: "",
+  contact: "",
+  address: "",
+  detail_address: "",
 };
 
 loadOrderProductInfo(order_products);
+loadOrdererInfo(orderer);
