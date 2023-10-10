2b75a5a92fece88a57bbd2f091a7c4213dc76e19
lee minseop <lms990108@gmail.com>
2023-10-10 18:44:26 +0900

    Feat: 주문 추가 시, token받아서 처리해서 userId 추출해서 추가

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const orderSchema = new Schema(
   {
-    user_email: {
+    userId: {
       type: String,
     },
     status: {
