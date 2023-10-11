e3a9f7fd19c41feb44feb21f9353271859f4b9e2
최정윤 <ej921004@naver.com>
2023-10-12 04:32:53 +0900

    Feat : 메인페이지 - 카테고리 별 상품 3개씩 미리보기

---
+++ views/mainPage/mainPage.js
@@ -0,0 +1,41 @@
+const keyboardContainer = document.querySelector("#keyboardContainer");
+const mouseContainer = document.querySelector("#mouseContainer");
+const soundContainer = document.querySelector("#soundContainer");
+const etcContainer = document.querySelector("#etcContainer");
+
+const url = "http://localhost:5001";
+
+const getThreeProducts = async (category, container) => {
+  await fetch(`${url}/api/product/?higherCategory=${category}&sortType=recent`)
+    .then((response) => {
+      if (!response.ok)
+        return response.json().then((data) => Promise.reject(data));
+      return response.json();
+    })
+    .then((data) => {
+      loadProductsContainer(data.slice(0, 3), container);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const loadProductsContainer = (products, container) => {
+  products.forEach((product) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      /*html*/ `
+  <div><a href="${url}/product?productNumber=${product.product_number}">
+  <img src=${product.main_img_url}>
+  <p class="is-size-5">${product.product_name}</p>
+  <p class="is-size-6">${parseInt(product.price).toLocaleString()}원</p></a>
+  </div>
+  `,
+    );
+  });
+};
+
+getThreeProducts("키보드", keyboardContainer);
+getThreeProducts("마우스", mouseContainer);
+getThreeProducts("음향장비", soundContainer);
+getThreeProducts("기타", etcContainer);
