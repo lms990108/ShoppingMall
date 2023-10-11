e61551b2383c460a5a158906d9c0a0a0e9322e2a
최정윤 <ej921004@naver.com>
2023-10-11 18:55:42 +0900

    Feat : 결제페이지 - 주문 요약 UI 추가

--- views/paymentPage/paymentPage.js
+++ views/paymentPage/paymentPage.js
@@ -4,6 +4,7 @@ const delivery_info = document.querySelector("#delivery_info");
 const order_summary = document.querySelector("#order_summary");
 const payment_method = document.querySelector("#payment_method");
 
+/* 주문 상품 정보 불러오기 */
 const orderProductHTML = (product) => {
   return /*html*/ `<div class="is-flex product">
   <div class="is-flex-grow-1">  <img class="product_img" src="${
@@ -16,6 +17,7 @@ const orderProductHTML = (product) => {
   </div>`;
 };
 
+/* 단일 상품 정보 컴포넌트 */
 const loadOrderProductInfo = (products) => {
   products.forEach((product) => {
     orderProduct_info.insertAdjacentHTML(
@@ -25,6 +27,7 @@ const loadOrderProductInfo = (products) => {
   });
 };
 
+/* 주문자 정보 불러오기 */
 const loadOrdererInfo = (orderer) => {
   orderer_info.insertAdjacentHTML(
     "beforeend",
@@ -37,7 +40,9 @@ const loadOrdererInfo = (orderer) => {
   );
 };
 
+/* 배송정보 불러오기 */
 const loadDeliveryInfo = (orderer) => {
+  /* 기본값은 주문자(사용자)와 동일 */
   delivery_info.insertAdjacentHTML(
     "beforeend",
     /*html*/ `
@@ -46,19 +51,22 @@ const loadDeliveryInfo = (orderer) => {
   <label class="label has-text-weight-bold">연락처   </label><input value=${orderer.contact} class="input-contact">
   <label class="label has-text-weight-bold">우편번호  </label><input value=${orderer.email} class="input-email">
   <label class="label has-text-weight-bold">주소  </label><input value=${orderer.address} class="input-address">
-  <label class="label has-text-weight-bold">상세주소  </label><input value=${orderer.detail_address} class="input-detail_address">
-  <label class="label has-text-weight-bold">배송메모  </label><select name="input-deliveryMemo" class="select">
+  <label class="label has-text-weight-bold">상세 주소  </label><input value=${orderer.detail_address} class="input-detail_address">
+  <label class="label has-text-weight-bold">배송 메모  </label>
+  <select name="input-deliveryMemo" class="select">
   <option>부재 시 경비실에 맡겨주세요.</option>
   <option>집 앞에 놔주세요.</option>
   <option>택배함에 놔주세요.</option>
   <option>배송 전에 꼭 연락주세요.</option>
   <option>직접입력</option>
   </select>
-  <label></label><input class="direct_input is-hidden"  placeholder="배송 시 요청사항을 입력해주세요.">
+  <label></label>
+  <input class="direct_input is-hidden"  placeholder="배송 시 요청사항을 입력해주세요.">
   </div>
   `,
   );
 
+  /* 주문자 정보와 동일 체크박스 선택 시 주문자와 동일 , 해제 시 직접 입력 */
   const checkbox = document.querySelector("#delivery_info .checkbox");
   checkbox.addEventListener("click", () => {
     document.querySelector(".input-name").value = checkbox.checked
@@ -78,6 +86,7 @@ const loadDeliveryInfo = (orderer) => {
       : "";
   });
 
+  /* 배송 메모의 직접 입력 옵션 선택 시 직접 입력 칸 보이기 */
   const select = document.querySelector("select");
   const direct_input = document.querySelector(".direct_input");
 
@@ -85,6 +94,28 @@ const loadDeliveryInfo = (orderer) => {
     if (select.value === "직접입력") direct_input.classList.remove("is-hidden");
     else direct_input.classList.add("is-hidden");
   });
+};
+
+/* 주문 요약 불러오기 */
+const loadOrderSumary = (order_products) => {
+  let product_price = 0;
+  let delivery_price = 0;
+
+  order_products.forEach(({ price, qty }) => {
+    product_price += price * qty;
+  });
+
+  const total_price = product_price + delivery_price;
+
+  order_summary.insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `<div class="py-5">
+  <div class="mx-5 my-3 is-flex is-justify-content-space-between"><label class="label has-text-weight-bold">상품 가격</label><span>${product_price.toLocaleString()} 원</span></div>
+  <div class="mx-5 my-3 is-flex is-justify-content-space-between"> <label class="label has-text-weight-bold">배송비</label></label><span class="has-text-grey-light">OPEN EVENT ★전상품 무료배송★</span><span>${delivery_price} 원</span></div>
+  <div class="mx-5 my-3 py-2 is-flex is-justify-content-space-between total_price"> <label class="label has-text-weight-bold">총 결제금액</label></label><span class="has-text-weight-bold">${total_price.toLocaleString()} 원</span>
+  </div></div>
+  `,
+  );
 };
 
 /* 주문 상품 mockData */
@@ -138,3 +169,4 @@ const delivery = {
 loadOrderProductInfo(order_products);
 loadOrdererInfo(orderer);
 loadDeliveryInfo(orderer);
+loadOrderSumary(order_products);
