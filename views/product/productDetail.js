cf4291ad26ca8680364f0b19663230bb12594094
chloe <imherejw@gmail.com>
2023-10-12 10:38:15 +0900

    Feat:  상단 스크롤 이벤트 및 수량,가격 조정 기능 추가

--- views/product/productDetail.js
+++ views/product/productDetail.js
@@ -1,6 +1,10 @@
 // 제품 상세 페이지
 // ex )http://localhost:5001/product_detail?productNumber=1
 
+// 가격 콤마 표시 함수 (3자리 단위로)
+function priceFormat(price) {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
+}
 
 // 상단으로 이동 버튼 스크롤 이벤트 (제품 상세 정보 들어올 시점에 등장!)
 const basicArea = window.innerHeight * 0.5;
@@ -28,32 +32,29 @@ let plus = document.querySelector(".plusbtn");
 let quantity = document.querySelector(".quantity");
 let totalprice = document.querySelector(".total_price");
 
-const countnum = 1;
+let countnum = 1;
 
 minus.addEventListener("click", () => {
 
-  if (countnum > 0) {
+  if (countnum > 1) {
     countnum -= 1; // - 버튼 클릭 시 마다 1씩 감소
-    quantity.textContent = countnum;
+    quantity.value = countnum;
 
     let totalcost = countnum * 249000; // 기본금액 임의 설정
     totalprice.textContent = totalcost.toLocaleString()
 
-  } else {
-    totalprice.textContent = 0 + "원"
-  }
+  } 
   
   
 })
-
-
 
 plus.addEventListener("click", () => {
   // + 버튼 클릭 시 마다 1씩 증가
   countnum += 1;
-  quantity.textContent = countnum;
+  quantity.value = countnum;
 
   let totalcost = countnum * 249000; // 기본금액 임의 설정
   totalprice.textContent = totalcost.toLocaleString()
 })
+
 
