d2cb72062a0e090c8b7cb73f59940b008cdb6dc7
muleesm <muleesm@gmail.com>
2023-10-12 13:58:57 +0900

    Feat: myPage에 로그아웃버튼 임시 추가

--- views/mypage/mypage.js
+++ views/mypage/mypage.js
@@ -158,4 +158,15 @@ const deleteAccountButton = document.getElementById('deleteAccountBtn');
   });
 
 
+  const logoutButton = document.getElementById('logoutButton'); // 로그아웃 버튼의 ID
+
+logoutButton.addEventListener('click', function() {
+    // 토큰 삭제
+    localStorage.removeItem('token');
+    alert("로그아웃 하셨습니다.")
+    // 메인 페이지로 리다이렉션
+    window.location.href = '/'; // 메인 페이지의 URL
+});
+
+
   
