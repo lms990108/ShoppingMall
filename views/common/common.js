f79b07b164204b8df8c9a713977cc783598bb6fc
최정윤 <ej921004@naver.com>
2023-10-12 23:59:30 +0900

    Refact : 네비게이터 카테고리 순서 정적으로 고정

--- views/common/common.js
+++ views/common/common.js
@@ -1,6 +1,6 @@
 // 모든 페이지에서 공통으로 사용되는 요소들을 정의하기 위한 파일입니다.
 
-import { getCategoryMap } from "../utils/catergoryHandler.js";
+import { getLowerCategory } from "../utils/catergoryHandler.js";
 
 /* head 태그 */
 /* favicon 적용, bulma 사용 */
@@ -51,38 +51,63 @@ header.insertAdjacentHTML(
             <div>
                 <a href="/cart"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M13.5 21c-.276 0-.5-.224-.5-.5s.224-.5.5-.5.5.224.5.5-.224.5-.5.5m0-2c-.828 0-1.5.672-1.5 1.5s.672 1.5 1.5 1.5 1.5-.672 1.5-1.5-.672-1.5-1.5-1.5m-6 2c-.276 0-.5-.224-.5-.5s.224-.5.5-.5.5.224.5.5-.224.5-.5.5m0-2c-.828 0-1.5.672-1.5 1.5s.672 1.5 1.5 1.5 1.5-.672 1.5-1.5-.672-1.5-1.5-1.5m16.5-16h-2.964l-3.642 15h-13.321l-4.073-13.003h19.522l.728-2.997h3.75v1zm-22.581 2.997l3.393 11.003h11.794l2.674-11.003h-17.861z"/></svg><a href="/mypage"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
                         <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm8.127 19.41c-.282-.401-.772-.654-1.624-.85-3.848-.906-4.097-1.501-4.352-2.059-.259-.565-.19-1.23.205-1.977 1.726-3.257 2.09-6.024 1.027-7.79-.674-1.119-1.875-1.734-3.383-1.734-1.521 0-2.732.626-3.409 1.763-1.066 1.789-.693 4.544 1.049 7.757.402.742.476 1.406.22 1.974-.265.586-.611 1.19-4.365 2.066-.852.196-1.342.449-1.623.848 2.012 2.207 4.91 3.592 8.128 3.592s6.115-1.385 8.127-3.59zm.65-.782c1.395-1.844 2.223-4.14 2.223-6.628 0-6.071-4.929-11-11-11s-11 4.929-11 11c0 2.487.827 4.783 2.222 6.626.409-.452 1.049-.81 2.049-1.041 2.025-.462 3.376-.836 3.678-1.502.122-.272.061-.628-.188-1.087-1.917-3.535-2.282-6.641-1.03-8.745.853-1.431 2.408-2.251 4.269-2.251 1.845 0 3.391.808 4.24 2.218 1.251 2.079.896 5.195-1 8.774-.245.463-.304.821-.179 1.094.305.668 1.644 1.038 3.667 1.499 1 .23 1.64.59 2.049 1.043z" /></svg></a>
-                <a class="button is-success is-light is-small" href="/login">로그인</a>
-
+                <a id="logButton" class="button is-success is-light is-small" href="/login">로그인</a>
             </div>
         </div>
     </section>
 </div>`,
 );
 
-/* Category nav 띄우기 위한 HTML 생성 */
-const generateCategoryHtml = (category) => {
-  let categoryHtml = "";
-  category.reverse().forEach(({ higher_category, lower_category }) => {
-    categoryHtml += `<div class="navbar-item is-hoverable has-dropdown">
-   <a class="navbar-link is-arrowless" href="/products?higherCategory=${higher_category.name}">${higher_category.name}</a>
-  <div class="navbar-dropdown">`;
-    lower_category.forEach((lowerItem) => {
-      categoryHtml += `<a class="navbar-item" href="/products?higherCategory=${higher_category.name}&lowerCategory=${lowerItem.name}">${lowerItem.name}</a>`;
+const logButton = document.querySelector("#logButton");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await fetch("http://localhost:5001/api/user", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
     });
-    categoryHtml += `</div>
-    </div>`;
+
+    if (response.ok) {
+      const userData = await response.json();
+      // console.log(userData);
+      logButton.innerHTML = "로그아웃";
+      logButton.className = "button is-danger is-light is-small";
+    } else {
+      console.error("Error fetching user data:", await response.json());
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+/* Category nav 띄우기 위한 HTML 생성 */
+const generateCategoryHtml = (higher_category_name, lower_category) => {
+  let categoryHtml = "";
+  categoryHtml += `<div class="navbar-item is-hoverable has-dropdown">
+   <a class="navbar-link is-arrowless" href="/products?higherCategory=${higher_category_name}">${higher_category_name}</a>
+  <div class="navbar-dropdown">`;
+  lower_category.forEach((lowerItem) => {
+    categoryHtml += `<a class="navbar-item" href="/products?higherCategory=${higher_category_name}&lowerCategory=${lowerItem.name}">${lowerItem.name}</a>`;
   });
+  categoryHtml += `</div>
+    </div>`;
   return categoryHtml;
 };
 
 /* category nav HTML 화면에 보이기 */
 const loadCategory = async () => {
-  const categoryData = await getCategoryMap();
+  const higher_category = ["키보드", "마우스", "음향장비", "기타"];
   const categoryDiv = document.querySelector(".navbar-start");
-  categoryDiv.insertAdjacentHTML(
-    "beforeend",
-    generateCategoryHtml(categoryData),
-  );
+  higher_category.forEach(async (higerItem) => {
+    const lower_category = await getLowerCategory("키보드");
+    categoryDiv.insertAdjacentHTML(
+      "beforeend",
+      generateCategoryHtml(higerItem, lower_category),
+    );
+  });
 };
 
 loadCategory();
